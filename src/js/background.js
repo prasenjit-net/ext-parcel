622e4948dd56e4fcc94b1f162d0498da5bee8573
Prasenjit Purohit <github@prasenjit.net>
2021-04-08 19:40:46 +0530

    Commit two

--- src/js/background.js
+++ src/js/background.js
@@ -4,3 +4,9 @@ const path = browser.runtime.getURL("path");
 console.log(path);
 
 console.log("Hello World");
+
+browser.runtime.onMessage.addListener(async (message, sender, responder) => {
+    console.log(message);
+    console.log(sender);
+    return {message: "You are welcome"};
+});
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,12 @@
 import browser from "webextension-polyfill";
+import "../styles/popup.scss";
 
 const pathUrl = browser.runtime.getURL("/path.html");
 console.log(pathUrl);
+
+const button = document.getElementById("send-message");
+button.addEventListener("click", () => {
+    browser.runtime.sendMessage({message: "Hello"}).then(resp => {
+        console.log(resp);
+    });
+});
